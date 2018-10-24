8fa98e56f6cb69cec4832088238b06362cfe03ec
Trevor F. Smith <trevor@trevor.smith.name>
2018-10-24 10:05:25 -0700

    Frame out the testing system, including tests, a runner, a TAP formatter for test results, and a vanilla JS results renderer that reacts to status events from the runner. Also includes a few naive but handy classes for mocking network activity like fetching. /example/ holds an example, natch.

---
+++ README.md
@@ -0,0 +1,5 @@
+# PotassiumES Testing Tools
+
+This will eventually be a test framework that we can use to test PotassiumES libraries and applications.
+
+You're welcome to look around, but #loweryourexpectations because at the moment this is mostly older code that I've pulled in from older versions of PotassiumES and started to clean up. - Trevor
---
+++ example/tests.js
@@ -0,0 +1,27 @@
+import Test from '../src/Test.js'
+import Runner from '../src/Runner.js'
+import SynchronousFetchMap from '../src/SynchronousFetchMap.js'
+import TestResultsRenderer from '../src/TestResultsRenderer.js'
+
+const tests = []
+
+tests.push(
+	new Test('Test 1', test => {
+		test.assertEqual(123, 123)
+		test.assertEqual('ssw', 'ssw')
+	})
+)
+
+tests.push(
+	new Test('Test 2', test => {
+		test.assertEqual(123, 'BOGUS')
+	})
+)
+
+tests.push(
+	new Test('Test 3', test => {
+		throw new Error('Nope!')
+	})
+)
+
+export { tests, Runner, TestResultsRenderer as Renderer }
